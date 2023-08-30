2fe359f89ef15bcbe09bf052b790f18b33d904ce
marshmello61 <ultramayur123@gmail.com>
2023-08-30 19:16:04 +0530

    xaga: textclassifier: add and ship prebuilt textclassifier
     - Helps fix the delay we have in copying text and that being available in clipboard
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>
    Signed-off-by: priiii1808 <priyanshusinghal0818@gmail.com>

--- device.mk
+++ device.mk
@@ -419,6 +419,13 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# TextClassifier
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/textclassifier/actions_suggestions.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/actions_suggestions.universal.model \
+    $(LOCAL_PATH)/textclassifier/textclassifier.en.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.en.model \
+    $(LOCAL_PATH)/textclassifier/lang_id.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/lang_id.model \
+    $(LOCAL_PATH)/textclassifier/textclassifier.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.universal.model
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
