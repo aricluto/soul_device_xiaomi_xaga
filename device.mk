3467ae0e6eb453bbf7edbca77fa706ad690cb4ba
ChrisCatto <chriscatto08@gmail.com>
2023-09-01 18:07:32 +0300

    xaga: Remove SGcamGOLite
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -160,10 +160,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0.vendor
 
-# SGcamGOLite
-PRODUCT_PACKAGES += \
-    SGcamGOLite
-
 # Gnss
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1.vendor \
