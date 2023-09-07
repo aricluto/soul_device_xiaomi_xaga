5d8397d12c7a5b2d2f8ef2170d0dc812381d1ff4
ChrisCatto <chriscatto08@gmail.com>
2023-09-07 12:11:22 +0300

    Revert "xaga: XiaomiParts: Drop Thermal Profiles"
    This reverts commit 840e75d126bc74fbf6e7574dd5e959385d8b0f20.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,6 +24,7 @@ import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -39,6 +40,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         } catch (Exception e) {
             Log.d(TAG, "Dirac is not present in system");
         }
+        ThermalUtils.startService(context);
         RefreshUtils.startService(context);        
     }
 }
