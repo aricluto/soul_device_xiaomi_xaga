9d119ff8cdb264cbfe1f23bcf4d399cb9ef43c0e
LuK1337 <priv.luk@gmail.com>
2023-09-08 10:53:09 +0300

    xaga: Set default Settings.Global.DEVICE_NAME according to MIUI
    MIUI uses ro.product.marketname for default device name.
    
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -278,6 +278,10 @@ PRODUCT_PACKAGES += \
     XagaProFrameworkResOverlay \
     XagaProInFrameworkResOverlay \
     SettingsProviderOverlayXaga \
+    SettingsProviderOverlayChina \
+    SettingsProviderOverlayChinaPlus \
+    SettingsProviderOverlayGlobal \
+    SettingsProviderOverlayIndia \
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
