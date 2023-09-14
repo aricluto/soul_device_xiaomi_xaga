dd8da3839f99af288445c683ae43a6f5ce814dea
bengris32 <bengris32@protonmail.ch>
2023-09-14 20:08:56 +0300

    xaga: Ship MediaTek cgroup/task_profiles configuration
    * MediaTek ship their own modified task_profiles and
      use the V30 cgroups configuration.
    
    Change-Id: I15245ea87b00925597db65f1564e808a094c937a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>
    Signed-off-by: priiii1808 <priyanshusinghal0818@gmail.com>

--- device.mk
+++ device.mk
@@ -116,6 +116,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5.vendor \
     android.hardware.camera.provider@2.6.vendor \
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    
 # Config store
 PRODUCT_PACKAGES += \
     disable_configstore
